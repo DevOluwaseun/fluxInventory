699868c46dcc56f61bd897947c9218bf77fcd075
DevOluwaseun <devoluwaseun@gmail.com>
2026-03-25 17:32:07 +0000

    feat: add product and product list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,63 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [data, setData] = useState("");
+  const [formData, setFormData] = useState({
+    productName: "",
+    price: "",
+    quantity: "",
+  });
+  const [catalog, setCatalog] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("http://localhost:3000/api")
-      .then(({ data }) => setData(data.message));
-  }, []);
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:3000/api", formData).then((res) => {
+      setCatalog(res.data);
+    });
+  };
 
   return (
     <div>
-      <h1>{data}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="productName"
+          value={formData.productName}
+          onChange={handleChange}
+          placeholder="Product Name"
+        />
+        <input
+          type="text"
+          name="price"
+          value={formData.price}
+          onChange={handleChange}
+          placeholder="Price"
+        />
+        <input
+          type="text"
+          name="quantity"
+          value={formData.quantity}
+          onChange={handleChange}
+          placeholder="quantity"
+        />
+        <button>Submit</button>
+      </form>
+
+      <ul>
+        {catalog.map((product, index) => (
+          <div key={index}>
+            <li> {product.productName}</li>
+            <li> {product.price}</li>
+            <li> {product.quantity}</li>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
--- server/main.js
+++ server/main.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = 3000;
-
+app.use(express.json());
 app.use(cors({ origin: "http://localhost:5174" }));
+
+let products = [];
 
 app.get("/", (req, res) => {
   res.send("Hello, World");
@@ -11,6 +13,13 @@ app.get("/", (req, res) => {
 
 app.get("/api", (req, res) => {
   res.json({ message: "Welcome to Flux inventory" });
+});
+
+app.post("/api", (req, res) => {
+  products.push(req.body);
+  console.log("Product added successfully");
+  res.json(products);
+  console.log(products);
 });
 
 app.listen(port, () => {
