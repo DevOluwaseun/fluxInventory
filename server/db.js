059e9227323dc28e85cbf7ae4a795cae1d9d70da
DevOluwaseun <devoluwaseun@gmail.com>
2026-04-13 08:23:13 +0100

    fix: add SSL to postgres connection for Railway

--- server/db.js
+++ server/db.js
@@ -5,12 +5,19 @@ const { Pool } = pkg;
 
 dotenv.config();
 
+// const pool = new Pool({
+//   user: "postgres",
+//   host: process.env.DATABASE_HOST,
+//   database: process.env.DATABASE_NAME,
+//   password: process.env.DATABASE_PASSWORD,
+//   port: process.env.PORT,
+// });
+
 const pool = new Pool({
-  user: "postgres",
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
